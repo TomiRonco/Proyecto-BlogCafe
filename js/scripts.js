6030cb7e9ac1fc121b56b9b6fb430c6a532a5743
Tomas Roncoroni <116822644+TomiRonco@users.noreply.github.com>
2023-05-14 20:30:21 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,125 @@
+// Seleccionar elementos...
+
+
+
+
+// Eventos en JavaScript...
+
+// Hay muchos eventos ocurriendo en tus sitios y aplicaciones web, cuando un usuario da click, cuando dan scroll, al presionar en un botón, enviar un formulario, pero uno de los más comunes es esperar a que el documento este listo...
+
+
+
+console.log('1');
+window.addEventListener('load', function() { // Cuando el Archivo JS y los archivos dependientes han cargado como es el HTML y las imagenes...
+    console.log('2');
+});
+
+window.onload = function() {
+    console.log('3')
+};
+
+document.addEventListener('DOMContentLoaded', function() { // Este se ejecuta cuando el HTML ha sido descargado pero no espera por CSS o imagenes...
+    console.log('4');
+});
+
+console.log('5');
+
+// Estos closures también pueden ser con una función aparte...
+
+
+
+// // Eventos Scroll...
+// window.onscroll = function(e) {
+//     console.log('scrolling...');
+
+//     console.log(this.scrollY);
+// }
+
+
+
+// Eventos con Click y submit...
+
+// const btnEnviar = document.querySelector('.formulario input[type=submit]');
+// console.log(btnEnviar);
+
+// btnEnviar.addEventListener('click', function() { // callback o closure 
+//     console.log('click');
+// });
+
+const datos = {
+    nombre: '',
+    email: '',
+    mensaje: ''
+}
+
+// submit
+const formulario = document.querySelector('.formulario');
+
+formulario.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    console.log(e);
+
+    console.log('Di click y la página ya no recarga');
+
+    console.log(datos);
+
+    // Validar el Formulario...
+
+    const { nombre, email, mensaje } = datos;
+
+    if(nombre === '' || email === '' || mensaje === '' ) {
+        console.log('Al menos un campo esta vacio');
+        mostrarError('Todos los campos son obligatorios');
+        return; // Detiene la ejecución de esta función
+    }
+
+    console.log('Todo bien...')
+
+    mostrarMensaje('Mensaje enviado correctamente');
+});
+
+
+function mostrarError(mensaje) {
+    const alerta = document.createElement('p');
+    alerta.textContent = mensaje;
+    alerta.classList.add('error');
+
+    formulario.appendChild(alerta);
+
+    setTimeout(() => {
+        alerta.remove();
+    }, 3000);
+}
+
+function mostrarMensaje(mensaje) {
+    const alerta = document.createElement('p');
+    alerta.textContent = mensaje;
+    alerta.classList.add('correcto');
+    formulario.appendChild(alerta);
+
+    setTimeout(() => {
+        alerta.remove();
+    }, 3000);
+}
+
+
+// Eventos de los Inputs...
+const nombre = document.querySelector('#nombre');
+const email = document.querySelector('#email');
+const mensaje = document.querySelector('#mensaje');
+
+
+nombre.addEventListener('input', leerTexto);
+email.addEventListener('input', leerTexto);
+mensaje.addEventListener('input', leerTexto);
+
+function leerTexto(e) {
+    // console.log(e);
+    // console.log(e.target.value);
+
+    datos[e.target.id] = e.target.value;
+
+    console.log(datos);
+}
+
